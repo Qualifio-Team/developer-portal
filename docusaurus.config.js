6f0f931bf17065b14c6409b16c7d328d9972dbe8
lmelon <laurent.melon@qualifio.com>
2021-03-02 10:26:55 +0100

    Fix edit link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -90,12 +90,12 @@ module.exports = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/edit/master/website/',
+          editUrl: 'https://github.com/Qualifio-Team/developer-portal/tree/main/docs',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+          editUrl: 'https://github.com/Qualifio-Team/developer-portal/tree/main/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
